b0acc9f67d55db0aab31dd8954d91bc6219947ef
Mazharrais <mazharmajo72@gmail.com>
2023-09-03 10:49:24 +0500

    add new folder

---
+++ javascript/assignment4/app.js
@@ -0,0 +1,17 @@
+var array = [ 
+    [ ],
+    [1, 2 ],
+    [3, 4],
+    [5, 6 ]
+];
+console.log(array)
+
+
+let activities = [
+    ['Work', 9],
+    ['Eat', 1],
+    ['Commute', 2],
+    ['Play Game', 1],
+    ['Sleep', 7]
+];
+document.write(activities)
