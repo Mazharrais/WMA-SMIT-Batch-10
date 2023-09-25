26e79974d89d146972d85fd41d511fc31af0ca0b
Mazharrais <mazharmajo72@gmail.com>
2023-09-25 21:44:54 +0500

    added new files

--- javascript/assignment8/app.js
+++ javascript/assignment8/app.js
@@ -1,1 +1,28 @@
-
+var left = 0;
+function abc() {
+  
+    var daino = document.getElementById('daino');
+    console.log(event.keyCode)
+    if(event.keyCode === 39) {
+        left = left + 10;
+        daino.style.left = left + 'px'
+    }
+    if(event.keyCode === 37) {
+        left = left - 10;
+        daino.style.left = left + 'px'
+    }
+    // var top =
+    // function jump(){
+        if(event.keyCode === 38) {
+    //    top = top + 400;
+       daino.style.bottom = '250px'
+       setTimeout(function(){
+        daino.style.bottom = '10px'
+        }, 550);
+        // }
+        
+    }
+    
+}
+window.onkeydown = abc;
+  
