599dc981264cc284a7d3ec3195b7cb61b9b4fb06
Mazharrais <mazharmajo72@gmail.com>
2023-10-16 11:05:36 +0500

    addes new files

--- javascript/assignment9/app.js
+++ javascript/assignment9/app.js
@@ -1,13 +1,17 @@
 var text = document.getElementById('text')
+var val2="";
 function abc(val){
-//  if(val=== 'images/background pic 7.jpg' || 'images/download.jpg' ||'images/pic5.webp'){
-//     style.color = "white";
-//  } else {
-//     style.color = "black";
-//  }
+ val2 = val
     var image = document.getElementById("backgroundpic");
     console.log(val)
     image.style.backgroundImage = `url('${val}')`
+
+
+    if(val === 'iimages/julamukhi.jpg' || 'images/gathering.jpeg' ||'images/pic5.webp'){
+        text.style.color = "white";
+     } else {
+        text.style.color = "black";
+     }
 }
 
 function changeButton(){
@@ -19,4 +23,12 @@ function changeButton(){
         doc.style.backgroundColor ='grey'
                  
     }
+}
+function post(){
+    console.log(val2);
+    var doc = document.getElementById('post1')
+    doc.innerHTML += `<div style='background-image:url("${val2}")' class="post1" id="post1">
+    <h1>${text.value}</h1>
+</div>`
+text.value = ""
 }
