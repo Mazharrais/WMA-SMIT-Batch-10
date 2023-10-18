22af323bea4a8c293f7e077db4330f3f07c7baea
Mazharrais <mazharmajo72@gmail.com>
2023-10-18 16:59:03 +0500

    added files

---
+++ javascript/assignment10/app.js
@@ -0,0 +1,329 @@
+var showRoom ={
+    toyota : {
+        corolla: {
+      type:{
+        name : " Toyota corolla",
+        Model: 2023,
+        color: "Pearl White",
+        cityRegistration: "karachi",
+        price: "50,000,00",
+        transmission: "Automatic",
+        img: "images/corolla/corolla1.png",
+          }
+        },
+          fortuner:{
+            type:{
+                name:"Toyota Fortuner",
+                Model: 2023,
+                color: "White",
+                cityRegistration: "karachi",
+                price: "70,000,00",
+                transmission: "Automatic",
+                img: "images/corolla/fortuner.webp",
+                }
+            },
+          
+          landcruiser:{
+          type:{
+          name:"Toyota Landcruiser",
+          Model: 2023,
+          color: "Silver",
+          cityRegistration: "karachi",
+          price: "100,000,00",
+          transmission: "Automatic",
+          img: "images/corolla/landcruiser.png",
+          }
+              }
+            },
+      honda:{
+        city:{
+         type:{
+         name:"Honda City",
+          Model: 2023,
+          color: "Silver",
+          cityRegistration: "karachi",
+          price: "45,000,00",
+          transmission: "Automatic",
+          img: "images/Honda/city.png",
+            }
+        },
+        civic:{
+            type:{
+            name:"Honda Civic",
+             Model: 2023,
+             color: "white",
+             cityRegistration: "karachi",
+             price: "70,000,00",
+             transmission: "Automatic",
+             img: "images/Honda/civic.png",
+               }
+           },
+           fit:{
+            type:{
+            name:"Fit City",
+             Model: 2023,
+             color: "Blue",
+             cityRegistration: "karachi",
+             price: "65,000,00",
+             transmission: "Automatic",
+             img: "images/Honda/fit.png",
+               }
+           }
+      },   
+      kia:{
+      picanto:{
+        type:{
+            name:"Kia Picanto",
+            Model: 2023,
+            color: "yellow",
+            cityRegistration: "karachi",
+            price: "55,000,00",
+            transmission: "Automatic",
+            img: "images/Kia/picanto.png",   
+        }
+      },
+      sportage:{
+        type:{
+            name:"Kia Sportage",
+            Model: 2023,
+            color: "Smoke White",
+            cityRegistration: "karachi",
+            price: "80,000,00",
+            transmission: "Automatic",
+            img: "images/Kia/sportage.png",   
+        }
+      },
+      stonic:{
+        type:{
+            name:"Kia Stonic",
+            Model: 2023,
+            color: "Black",
+            cityRegistration: "karachi",
+            price: "70,000,00",
+            transmission: "Automatic",
+            img: "images/Kia/stonic.webp",   
+        }
+      }
+      },
+      suzuki:{
+        alto:{
+        type:{
+            name:"Suzuki Alto",
+            Model: 2023,
+            color: "Red",
+            cityRegistration: "karachi",
+            price: "32,000,00",
+            transmission: "Automatic",
+            img: "images/Suzuki/Alto.png",  
+        }
+        },
+        cultus:{
+            type:{
+                name:"Suzuki Cultus",
+                Model: 2023,
+                color: "Blue",
+                cityRegistration: "karachi",
+                price: "32,000,00",
+                transmission: "Automatic",
+                img: "images/Suzuki/cultus.webp",  
+            }
+            },
+            swift:{
+                type:{
+                    name:"Suzuki Swift",
+                    Model: 2023,
+                    color: "White",
+                    cityRegistration: "karachi",
+                    price: "50,000,00",
+                    transmission: "Automatic",
+                    img: "images/Suzuki/swift.png",  
+                }
+                }
+               },
+               hyundai:{
+                elantra:{
+                    type:{
+                        name:"Hyundai Elantra",
+                        Model: 2023,
+                        color: "Grey",
+                        cityRegistration: "karachi",
+                        price: "150,000,00",
+                        transmission: "Automatic",
+                        img: "images/Hyundai/Elantra.png",  
+                    }
+                    },
+                    sonata:{
+                        type:{
+                            name:"Hyundai Sonata 2.0",
+                            Model: 2023,
+                            color: "Black",
+                            cityRegistration: "karachi",
+                            price: "150,000,00",
+                            transmission: "Automatic",
+                            img: "images/Hyundai/sonata.png",  
+                        }
+                        },
+                        sonata:{
+                            type:{
+                                name:"Hyundai sonata 2.5",
+                                Model: 2023,
+                                color: "smoke Grey",
+                                cityRegistration: "karachi",
+                                price: "150,000,00",
+                                transmission: "Automatic",
+                                img: "images/Hyundai/sonata2.5.png",  
+                            }
+                            }
+                          },
+      nissan:{
+        dayz:{
+            type:{
+                name:"Nissan Dayz",
+                Model: 2023,
+                color: "Aqua",
+                cityRegistration: "karachi",
+                price: "50,000,00",
+                transmission: "Automatic",
+                img: "images/Nissan/dayz.png",  
+            }
+            },
+            leaf:{
+                type:{
+                    name:"Nissan Leaf",
+                    Model: 2023,
+                    color: "Light Grey",
+                    cityRegistration: "karachi",
+                    price: "75,000,00",
+                    transmission: "Automatic",
+                    img: "images/Nissan/leaf.png",  
+                }
+                },
+                dayz:{
+                    type:{
+                        name:"Nissan Dayz",
+                        Model: 2023,
+                        color: "Aqua",
+                        cityRegistration: "karachi",
+                        price: "50,000,00",
+                        transmission: "Automatic",
+                        img: "images/Nissan/dayz.png",  
+                    }
+                    },
+                    note:{
+                        type:{
+                            name:"Nissan Note",
+                            Model: 2023,
+                            color: "Get Black",
+                            cityRegistration: "karachi",
+                            price: "150,000,00",
+                            transmission: "Automatic",
+                            img: "images/Nissan/note.webp",  
+                        }
+                        }
+                      },
+        heavyVehicle:{
+            forland:{
+                type:{
+                    name:"Forland",
+                    Model: 2023,
+                    color: "Pearl White",
+                    cityRegistration: "karachi",
+                    price: "80,000,00",
+                    transmission: "Automatic",
+                    img: "images/Heavy vehicle/forland.png",  
+                }
+                },
+                hino:{
+                    type:{
+                        name:"Hino",
+                        Model: 2023,
+                        color: "Pearl White",
+                        cityRegistration: "karachi",
+                        price: "80,000,00",
+                        transmission: "Automatic",
+                        img: "images/Heavy vehicle/Hino.png",  
+                    }
+                    },
+                    porter:{
+                        type:{
+                            name:"Hyundai Porter",
+                            Model: 2023,
+                            color: "Pearl White",
+                            cityRegistration: "karachi",
+                            price: "80,000,00",
+                            transmission: "Automatic",
+                            img: "images/Heavy vehicle/porter.png",  
+                        }
+                        }
+                       },              
+
+}
+var company = document.getElementById("company")
+var brand = document.getElementById("brand")
+var allCars = document.getElementById("allCars")
+company.innerHTML = `<option value="">select</option>`
+brand.innerHTML = `<option value="">select</option>`
+
+
+
+
+for ( var key in showRoom){
+    company.innerHTML += `
+    <option value="${key}">${key.toUpperCase()}</option>
+    `
+    for (var key1 in showRoom[key]){
+    for (var key2 in showRoom[key][key1]){
+    var carData = showRoom[key][key1][key2];
+        allCars.innerHTML +=`
+        <div class="col mb-2">
+        <div class="card">
+        <img src="${ carData.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${ carData.name || "unknown"}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">${ carData.Model}</h6>
+             <h2>Rs${ carData.price}</h2>
+              
+         
+
+
+
+             
+            </div>
+          </div>
+          </div>
+        `
+    }    
+    }
+
+}
+
+function onCompanyChange(){
+    brand.innerHTML = "";
+    brand.innerHTML = `<option value="">select</option>` 
+    for(var key in showRoom[company.value]){
+        // console.log(key)
+        brand.innerHTML +=`
+        <option value="${key}">${key.toUpperCase()}</option> 
+        `
+    }
+}
+
+function filterCars(){
+    
+    var carData = showRoom[company.value][brand.value].type
+    allCars.innerHTML =`
+    <div class="col mb-2">
+    <div class="card">
+    <img src="${carData.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${carData.name || "unknown"}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">${carData.Model}</h6>
+         <h2>Rs${carData.price}</h2>
+          
+         
+        </div>
+      </div>
+      </div>
+    `
+}
+
