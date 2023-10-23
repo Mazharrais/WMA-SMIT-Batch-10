06d7372c916f3fab895c0a5459e182a184b3edf6
Mazharrais <mazharmajo72@gmail.com>
2023-10-23 19:49:13 +0500

    added new code

---
+++ javascript/assinment11/app.js
@@ -0,0 +1,373 @@
+var questionsList  = [
+    {
+        question : "The S.I unit of Heat is...?",
+        option1: "Watt",
+        option2 : "Volt",
+        option3 : "Joule",
+        option4 : "Newton",
+        answer : 'Joule'
+    
+    },
+
+    {
+        question : "Physics is one of the branches of...?",
+        option1 : "Physical sciences",
+        option2 : "Biological sciences",
+        option3 : "Social science",
+        option4 : "Life sciences branch",
+        answer : 'Physical sciences'
+    
+    },
+
+    {
+        question : "The branch of science which deals with the properties of matter and energy is called...?",
+        option1 : "Biology",
+        option2  : "Biology",
+        option3 : "Physics",
+        option4 : "Chemistry",
+        answer :  'Physics'
+    
+    },
+
+    // {
+    //     question : "Which branch of science plays an important role in engineering...?",
+    //     option1 : "Biology",
+    //     option2 : "Biology",
+    //     option3 : "Physics",
+    //     option4 : "Chemistry",
+    //     answer : 'Physics'
+    
+    // },
+
+    // {
+    //     question : "The Branch of Physics deals with highly energetic ions is called...?",
+    //     option1 : "Elementary articles",
+    //     option2 : "Article physics",
+    //     option3 : "Ionic physics",
+    //     option4 : "Plasma physics",
+    //     answer : 'Plasma physics'
+    
+    // },
+
+    // {
+    //     question : "The physics of moving charge particles is known as...?",
+    //     option1 : "Plasma physics",
+    //     option2 : "Electro-static’s",
+    //     option3 : "Electro-magnetic",
+    //     option4 : "Electro-dynamics",
+    //     answer : 'Electro-dynamics'
+    
+    // },
+
+    // {
+    //     question : "The first book of Physics was written by...?",
+    //     option1 : "Kelvin",
+    //     option2 : "Newton",
+    //     option3 : "Aristotle",
+    //     option4 : "Faraday",
+    //     answer : 'Aristotle'
+    
+    // },
+
+    // {
+    //     question : "The co efficient of linear expansion is equal to...?",
+    //     option1 : "ΔL = αLΔT",
+    //     option2 : "ΔL/LΔT",
+    //     option3 : "LΔT/ΔL",
+    //     option4 : "αLΔT-ΔL",
+    //     answer : 'ΔL = αLΔT'
+    
+    // },
+
+    // {
+    //     question : "linear expansion occurs in...?",
+    //     option1 : "solid",
+    //     option2 : "liquid",
+    //     option3 : "both",
+    //     option4 : "none",
+    //     answer : 'solid'
+    
+    // },
+
+    // {
+    //     question : "When body is in motion, ____________ always changes...?",
+    //     option1 : "its velocity",
+    //     option2 : "its acceleration",
+    //     option3 : "its position vector",
+    //     option4 : "its momentum",
+    //     answer : 'its position vector'
+    
+    // },
+
+    // {
+    //     question : "A man is in a car is moving with velocity of 36km/hr. His speed with repect to the car is...?",
+    //     option1 : "10m/s",
+    //     option2 : "36m/s",
+    //     option3 : "zero",
+    //     option4 : "infinite",
+    //     answer : 'zero'
+    
+    // },
+
+    // {
+    //     question : "Area under velocity time graph represent...?",
+    //     option1 : "force",
+    //     option2 : "displacement",
+    //     option3 : "distance",
+    //     option4 : "acceleration",
+    //     answer : 'displacement'
+    
+    // },
+
+    // {
+    //     question : "1st law of motion gives the definition of...?",
+    //     option1 : "rest",
+    //     option2 : "motion",
+    //     option3 : "velocity",
+    //     option4 : "force",
+    //     answer : 'force'
+    
+    // },
+
+    // {
+    //     question : "The dimension of force is...?",
+    //     option1 : "MLT-2",
+    //     option2 : "ML2T-2",
+    //     option3 : "ML2T2",
+    //     option4 : "ML-2T-2",
+    //     answer : 'MLT-2'
+    
+    // },
+
+    // {
+    //     question : "Taking off rocket can be explained by...?",
+    //     option1 : "1st law of motion",
+    //     option2 : "2nd law of motion",
+    //     option3 : "Law of conservation of momentum",
+    //     option4 : "law of conservation of energy",
+    //     answer : 'Law of conservation of momentum'
+    
+    // },
+
+    // {
+    //     question : "Distance covered by a freely falling body in 2 seconds will be...?",
+    //     option1 : "4.9 m",
+    //     option2 : "19.6 m",
+    //     option3 : "39.2 m",
+    //     option4 : "44.2 m",
+    //     answer : '19.6 m'
+    
+    // },
+
+    // {
+    //     question : "Time rate of change of momentum is equal to_...?",
+    //     option1 : "force",
+    //     option2 : "impulse",
+    //     option3 : "velocity",
+    //     option4 : "both A and C",
+    //     answer : 'force'
+    
+    // },
+
+    // {
+    //     question : "A body is moving with uniform velocity. Its...?",
+    //     option1 : "speed changes",
+    //     option2 : "acceleration changes",
+    //     option3 : "direction of motion changes",
+    //     option4 : "displacement from origin changes",
+    //     answer : 'displacement from origin changes'
+    
+    // },
+
+    // {
+    //     question : "A body is moving with uniform velocity. Its...?",
+    //     option1 : "speed changes",
+    //     option2 : "acceleration changes",
+    //     option3 : "direction of motion changes",
+    //     option4 : "displacement from origin changes",
+    //     answer : 'displacement from origin changes'
+    
+    // },
+
+    // {
+    //     question : "2nd law of motion gives the definition of...?",
+    //     option1 : "force",
+    //     option2 : "acceleration",
+    //     option3 : "velocity",
+    //     option4 : "both force and acceleration",
+    //     answer : 'acceleration'
+    
+    // },
+
+    // {
+    //     question : "Momentum depends upon_...?",
+    //     option1 : "force acts on the body",
+    //     option2 : "mass of the body",
+    //     option3 : "velocity of the body",
+    //     option4 : "both mass and velocity of the body",
+    //     answer : 'both mass and velocity of the body'
+    
+    // },
+
+    // {
+    //     question : "During long jump, athlete runs before taking the jump. By doing so he_...?",
+    //     option1 : "provide him a larger inertia",
+    //     option2 : "decreases his inertia",
+    //     option3 : "increases his momentum",
+    //     option4 : "decreases his momentum",
+    //     answer : 'increases his momentum'
+    
+    // },
+
+    // {
+    //     question : "Which of the following is not an example of projectile motion _...?",
+    //     option1 : "a gas filled ballon",
+    //     option2 : "bullet fired from gun",
+    //     option3 : "a football kicked",
+    //     option4 : "a base ball shot",
+    //     answer :  'a gas filled ballon'
+    
+    // },
+
+    // {
+    //     question : "The distance between any two consecutive crests or troughs is called..?",
+    //     option1 : "frequency",
+    //     option2 : "period",
+    //     option3 : "Wave length",
+    //     option4 : "phase difference",
+    //     answer : 'Wave length'
+    
+    // },
+
+    // {
+    //     question : "In vibrating cord the points where the amplitude is maximum, are called...?",
+    //     option1 : "antinodes",
+    //     option2 : "nodes",
+    //     option3 : "troughs",
+    //     option4 : "crests",
+    //     answer : 'antinodes'
+    
+    // },
+]
+
+
+// var index = 0;
+// var score = 0;
+// function renderQuestion(){
+//     var question = document.getElementById("question");
+//     var options = document.getElementsByName("option")
+
+//     for (var i=0; i<options.length; i++){
+//         if(options[i].checked){
+            
+//             if(options[i].value === questionsList[index -1].answer){
+//                 console.log(options[i].value,questionsList[index -1].answer);
+//                 score++
+//             }
+            
+//         }
+//     }
+//     if(! questionsList[index]){
+//         console.log("Result" + score);
+//         return;
+//     }
+ 
+
+//     function showScore(){
+//         resetState();
+//         question.innerHTML = `Your Score ${score} Out of ${questionsList.length}!`;
+   
+//      }
+
+//     if(questionsList[index]){
+
+    
+//     question.innerHTML = 
+//     ` <p>${questionsList[index].question}</p>
+//     <div>
+//         <label><input type="radio" name="option" value="${questionsList[index].option1}">${questionsList[index].option1}</label>
+//         </div>
+//         <div>
+//         <label><input type="radio" name="option" value="${questionsList[index].option2}">${questionsList[index].option2}</label>
+//         </div>
+//         <div>
+//         <label><input type="radio" name="option" value="${questionsList[index].option3}">${questionsList[index].option3}</label>
+//         </div>
+//         <div>
+//         <label><input type="radio" name="option" value="${questionsList[index].option4}">${questionsList[index].option4}</label>
+//     </div>
+//     `
+
+//     index++
+   
+  
+// }
+//  }
+
+// renderQuestion()
+    
+
+var question = document.querySelector('.question');
+var option1 = document.querySelector('#option1');
+var option2 = document.querySelector('#option2');
+var option3 = document.querySelector('#option3');
+var option4 = document.querySelector('#option4');
+var submit = document.querySelector('#submit');
+var answers = document.querySelectorAll('.answer');
+var showScore = document.querySelector('#showScore');
+
+
+var index = 0;
+var score = 0;
+function loadQuestion(){
+    var allQuestion = questionsList[index];
+    question.innerText = allQuestion.question;
+    option1.innerText = allQuestion.option1;
+    option2.innerText = allQuestion.option2;
+    option3.innerText = allQuestion.option3;
+    option4.innerText = allQuestion.option4;
+
+}
+loadQuestion()
+
+
+function getCheckAnswer(){
+    var answer;
+    answers.forEach((curAnsElem)=>{
+      if(curAnsElem.checked){
+        answer = curAnsElem.id
+      }
+     
+    })  
+    return answer;
+};
+
+function deselectAll(){
+   answers.forEach((curAnsElem) => curAnsElem.checked = false);
+}
+
+
+submit.addEventListener('click', () =>{
+    var checkedAnswer = getCheckAnswer();
+   console.log(checkedAnswer);
+
+   if(checkedAnswer === questionsList[index].answer){
+    score++;
+   };
+    index++;
+
+    deselectAll();
+
+    
+    if(index < questionsList.length){
+        loadQuestion();
+    }else{
+        showScore.innerHTML = `
+      <h3> You Scored ${score}/${questionsList.length}  </h3>
+      <button class="btn" onclick="location.reload()">Play Again </button>
+      
+      `;
+      showScore.classList.remove('scoreArea')
+    }
+
+});
